6b1307d26a235ee19210a47d9f92484c833ade64
Sasa Kocic <sasa.kocic@gmail.com>
2019-04-20 07:38:13 +0200

    serverCreationStatus

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => {
@@ -23,4 +24,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
