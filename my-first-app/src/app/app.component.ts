f34ba65af0fbe6e443f4f933651372b89c4f0f42
Sasa Kocic <sasa.kocic@gmail.com>
2019-04-20 09:12:05 +0200

    assignment

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -8,10 +8,20 @@ import { Component } from '@angular/core';
     h3 {
       color: dodgerblue;
     }
+    .white-text {
+      color: white;
+    }
   `]
 })
 
 export class AppComponent {
   title = 'my-first-app';
   username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
