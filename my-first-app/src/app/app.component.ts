399762cd072670a47513230ea3b16939503b170f
Sasa Kocic <sasa.kocic@gmail.com>
2019-04-20 09:14:30 +0200

    index in ngFor

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -22,6 +22,7 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
