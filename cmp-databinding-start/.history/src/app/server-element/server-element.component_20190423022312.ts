162d303dac96ed42a90fc4a981ebe982814e7f64
Sasa Kocic <sasa.kocic@gmail.com>
2019-04-23 02:23:48 +0200

    view encapsulation none

--- cmp-databinding-start/.history/src/app/server-element/server-element.component_20190423022312.ts
+++ cmp-databinding-start/.history/src/app/server-element/server-element.component_20190423022312.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: false
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {
