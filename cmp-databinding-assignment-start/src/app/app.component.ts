93ac79e7d111f2e9aca31232ad0c044835c4a1bc
Sasa Kocic <sasa.kocic@gmail.com>
2019-04-24 22:37:09 +0200

    finish

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers = [];
+  evenNumbers = [];
+
+  onTriggerFired(firedNumber: number) {
+    console.log('onTriggerFired ' + firedNumber);
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 }
