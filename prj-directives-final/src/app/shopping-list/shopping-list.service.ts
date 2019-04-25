da957475abc89627c8d46e039a87685b5fefe12d
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-25 17:24:42 +0200

    115. Using Services for Pushing Data

--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+// @Injectable({
+//   providedIn: 'root'
+// })
 export class ShoppingListService {
+  ingredientChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
   constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientChanged.emit(this.getIngredients());
+  }
 }
