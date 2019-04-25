611dee24d5cf8c0a2c24b92ad8f815dc82e0ecaf
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-25 15:04:17 +0200

    103-105. Creating a Data Service

---
+++ services-start/src/app/account/accounts.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+
+  constructor() { }
+}
