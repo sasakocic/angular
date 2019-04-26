0db77b8e8586eb528667ed5be41abd89ca4372d3
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-26 14:15:37 +0200

    127. Passing Parameters to Routes

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -19,6 +19,6 @@ export class ServersComponent implements OnInit {
   }
 
   onReload() {
-    this.router.navigate(['servers'], {relativeTo: this.activatedRoute});
+    // this.router.navigate(['servers'], {relativeTo: this.activatedRoute});
   }
 }
