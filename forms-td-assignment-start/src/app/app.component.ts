814237d7f8b195b81afae6bf5f930c4e3a17eeb9
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-29 18:16:22 +0200

    assignment 6

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild('signupForm') sgnForm: NgForm;
 
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  selectedSubscription = 'Advanced';
+
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 }
