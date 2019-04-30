a63ead567e545e3f59452b98ae4eb2067fd26535
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-30 09:38:09 +0200

    197. Creating a Form Module

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+    });
+  }
   genders = ['male', 'female'];
   signupForm: FormGroup;
 }
