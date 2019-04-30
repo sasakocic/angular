ed6d0b9b001fca0b5bda545cd04610a145918958
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-30 11:28:25 +0200

    207. Reactive: Reacting to Status or Value Changes

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -17,6 +17,10 @@ export class AppComponent implements OnInit {
       'gender': new FormControl("male", Validators.required),
       'hobbies': new FormArray([])
     });
+    this.signupForm.valueChanges.subscribe(
+      (value) => console.log(value)
+    );
+    this.signupForm.statusChanges.subscribe((status) => console.log(status));
   }
   genders = ["male", "female"];
   signupForm: FormGroup;
