b1a042a4a11a59900326aa890139b1a1c75f5980
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-30 13:32:41 +0200

    216. Allowing the User to Clear (Cancel) the Form

--- prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -53,4 +53,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
 }
