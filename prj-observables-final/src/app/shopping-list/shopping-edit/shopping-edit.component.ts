0fadb7c73c31045feeba383d9f2ae127ec037f57
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-04-30 13:10:20 +0200

    212. Allowing the Selection of Items in the List

--- prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,21 +1,33 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  OnDestroy
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editItemIndex: number;
+
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editItemIndex = index;
+      }
+    );
   }
 
   onAddItem(form: NgForm) {
@@ -24,4 +36,7 @@ export class ShoppingEditComponent implements OnInit {
     this.slService.addIngredient(newIngredient);
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
