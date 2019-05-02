c6381eec706ccbc2df37ce635a4518fe55eb1d1a
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 14:13:21 +0200

    267. Signing Users Up

--- prj-forms-final/src/app/auth/signup/signup.component.ts
+++ prj-forms-final/src/app/auth/signup/signup.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSignup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
+    console.log(email);
+    this.authService.signupUser(email, password);
   }
 }
