0c4a53fa1abf8ea54242c91cbcde191089da6560
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 12:29:45 +0200

    258. Sending PUT Request to Save Data

---
+++ prj-forms-final/src/app/shared/data-storage.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+  constructor(
+    private http: Http,
+    private recipeService: RecipeService
+  ) { }
+
+  storeRecipes() {
+    return this.http.put(
+      'https://ng-recipe-book-aa489.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+}
