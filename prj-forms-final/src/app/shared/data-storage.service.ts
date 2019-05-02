2147d279d9d5c6ec18840cb91a55b6935b582688
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 12:45:51 +0200

    260.. Transforming Response Data to Prevent Errors

--- prj-forms-final/src/app/shared/data-storage.service.ts
+++ prj-forms-final/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import 'rxjs/Rx';
 
 @Injectable({
   providedIn: 'root'
@@ -18,11 +19,20 @@ export class DataStorageService {
   }
 
   getRecipes() {
-    return this.http.get('https://ng-recipe-book-aa489.firebaseio.com/recipes.json').subscribe(
-      (response: Response) => {
+    return this.http
+      .get("https://ng-recipe-book-aa489.firebaseio.com/recipes.json")
+      .map((response: Response) => {
         const recipes: Recipe[] = response.json();
+        for (let recipe of recipes) {
+          if (!recipe["ingredient"]) {
+            console.log(recipe);
+            recipe["ingredient"] = [];
+          }
+        }
+        return recipes;
+      })
+      .subscribe((recipes: Recipe[]) => {
         this.recipeService.setRecipes(recipes);
-      }
-    );
+      });
   }
 }
