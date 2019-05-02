7ccd8b569b0bc269b22d059b78e9ae4e0b1517f2
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 17:39:21 +0200

    284. Creating the Shared Module

--- authentication-final/src/app/recipes/recipes.module.ts
+++ authentication-final/src/app/recipes/recipes.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+
 import { RecipesComponent } from './recipes.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { DropdownDirective } from '../shared/dropdown.directive';
-import { CommonModule } from '@angular/common';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+    SharedModule
   ]
 })
 export class RecipesModule {
