5aaa55f36872fa6b794680a9e651a30a97caff97
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 14:03:52 +0200

    265. Setting Up the Firebase SDK

--- prj-forms-final/src/app/app.component.ts
+++ prj-forms-final/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit {
+  loadedFeature = "recipe";
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCvckLTDnDH9NkiPKTTXqT9OAhvFFY0V0Y",
+      authDomain: "ng-recipe-book-aa489.firebaseio.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
