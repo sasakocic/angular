e847693ab8f8c7cc36514cced5bdb8f66848273c
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 14:37:48 +0200

    270. Sending the Token

--- prj-forms-final/src/app/shared/data-storage.service.ts
+++ prj-forms-final/src/app/shared/data-storage.service.ts
@@ -3,24 +3,30 @@ import { Http } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import 'rxjs/Rx';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataStorageService {
   constructor(
     private http: Http,
-    private recipeService: RecipeService
-  ) { }
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
+    const token = this.authService.getToken();
     return this.http.put(
-      'https://ng-recipe-book-aa489.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+      "https://ng-recipe-book-aa489.firebaseio.com/recipes.json?auth=" + token,
+      this.recipeService.getRecipes()
+    );
   }
 
   getRecipes() {
+    const token = this.authService.getToken();
     return this.http
-      .get("https://ng-recipe-book-aa489.firebaseio.com/recipes.json")
+      .get("https://ng-recipe-book-aa489.firebaseio.com/recipes.json?auth="+ token)
       .map((response: Response) => {
         const recipes: Recipe[] = response.json();
         for (let recipe of recipes) {
