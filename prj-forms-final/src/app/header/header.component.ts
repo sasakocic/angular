b8cf53f6f1e42b07906f5d7afa2682689410cd3c
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-02 14:43:16 +0200

    271. Checking and Using Authentication Status

--- prj-forms-final/src/app/header/header.component.ts
+++ prj-forms-final/src/app/header/header.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   onSaveData() {
-    this.dataStorageService.storeRecipes().subscribe(
-        (response: Response) => {
-          console.log(response);
-        }
-    );
+    this.dataStorageService.storeRecipes().subscribe((response: Response) => {
+      console.log(response);
+    });
   }
 
   onFetchData() {
