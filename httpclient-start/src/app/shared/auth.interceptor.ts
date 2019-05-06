f8e26c6fc597e72512177b6be49840681959019b
Saša Kocić <sasa.kocic@yogaeasy.de>
2019-05-06 12:08:26 +0200

    314. Interceptors

---
+++ httpclient-start/src/app/shared/auth.interceptor.ts
@@ -0,0 +1,11 @@
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Intercepted!', req);
+    return next.handle(req);
+    // return null;
+  }
+
+}
